ec62f908a140c47f548e587d7d2c7fa900267fae
Codename;0 <139770129+codename0og@users.noreply.github.com>
2024-12-17 05:54:16 +0100

    ✨

--- tabs/settings/settings.py
+++ tabs/settings/settings.py
@@ -4,17 +4,14 @@ import sys
 now_dir = os.getcwd()
 sys.path.append(now_dir)
 
-from tabs.settings.sections.presence import presence_tab
 from tabs.settings.sections.precision import precision_tab
 #from tabs.settings.sections.themes import theme_tab
-#from tabs.settings.sections.version import version_tab
 #from tabs.settings.sections.lang import lang_tab
 from tabs.settings.sections.restart import restart_tab
 from tabs.settings.sections.model_author import model_author_tab
 
 
 def settings_tab():
-    presence_tab()
     precision_tab()
 #    theme_tab()
 #    lang_tab()
