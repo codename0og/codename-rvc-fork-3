acd8af57480f07bb950fa119097d5bdb46e77f77
Codename;0 <139770129+codename0og@users.noreply.github.com>
2025-01-11 05:35:51 +0100

    Update settings.py

--- tabs/settings/settings.py
+++ tabs/settings/settings.py
@@ -1,19 +1,14 @@
 import os
 import sys
+import gradio as gr
 
 now_dir = os.getcwd()
 sys.path.append(now_dir)
 
-from tabs.settings.sections.precision import precision_tab
-#from tabs.settings.sections.themes import theme_tab
-#from tabs.settings.sections.lang import lang_tab
 from tabs.settings.sections.restart import restart_tab
 from tabs.settings.sections.model_author import model_author_tab
 
 
 def settings_tab():
-    precision_tab()
-#    theme_tab()
-#    lang_tab()
     restart_tab()
     model_author_tab()
