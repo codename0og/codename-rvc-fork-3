ce28ea2b58f633255a74d7d1a3abc19b1114e6f7
Codename;0 <139770129+codename0og@users.noreply.github.com>
2025-05-16 02:56:58 +0200

    safety for M2 and perhaps other chips..?

--- run-fork.sh
+++ run-fork.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-printf "\033]0;Codename-RVC-Fork\007"
+printf "\033]0;Applio\007"
 . .venv/bin/activate
 
- export PYTORCH_ENABLE_MPS_FALLBACK=1
- export PYTORCH_MPS_HIGH_WATERMARK_RATIO=0.0
- 
+export PYTORCH_ENABLE_MPS_FALLBACK=1
+export PYTORCH_MPS_HIGH_WATERMARK_RATIO=0.0
+
+# Uncomment the one below in case of inference issues on idk.. M1, M2 or M3? chips.
+#export OMP_NUM_THREADS=1
+
 clear
 python app.py --open
